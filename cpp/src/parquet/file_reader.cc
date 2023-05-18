66ad506e381d8d2053eb1e30ea3df2c89f3a7c5b
Jack Del Vecchio <jackdelv39@gmail.com>
2023-05-18 14:51:46 +0000

    Try to clean up resources after execution.

--- cpp/src/parquet/file_reader.cc
+++ cpp/src/parquet/file_reader.cc
@@ -296,6 +296,22 @@ class SerializedFile : public ParquetFileReader::Contents {
 
   void Close() override {
     if (file_decryptor_) file_decryptor_->WipeOutDecryptionKeys();
+    
+    // Cleanup some resources
+    if(source_) {
+      free(source_.get());
+      source_ = nullptr;
+    }
+
+    if(cached_source_) {
+      free(cached_source_.get());
+      cached_source_ = nullptr;
+    }
+    
+    if(file_metadata_) {
+      free(file_metadata_.get());
+      file_metadata_ = nullptr;
+    }
   }
 
   std::shared_ptr<RowGroupReader> GetRowGroup(int i) override {
